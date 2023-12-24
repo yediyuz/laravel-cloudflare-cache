c8a0fcc2692ecbb782343b1249840648e35b750c
Mert <mertemreasan@gmail.com>
2023-12-24 03:02:43 +0300

    add static methods to docblock

--- src/Facades/CloudflareCache.php
+++ src/Facades/CloudflareCache.php
@@ -7,6 +7,13 @@ use Illuminate\Support\Facades\Facade;
 
 /**
  * @see \Yediyuz\CloudflareCache\CloudflareCache
+ *
+ * @method static bool        isEnabled()
+ * @method static bool|string purgeEverything()
+ * @method static bool|string purgeByPrefixes(array $prefixes)
+ * @method static bool|string purgeByTags(array $tags)
+ * @method static bool|string purgeByHosts(array $hosts)
+ * @method static bool|string purgeByUrls(array $urls)
  */
 class CloudflareCache extends Facade
 {
